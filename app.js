103ec3bbe8efde3b25d179712bc05292ecb86932
0xsamuel-eth <sammmastenn@gmail.com>
2023-08-02 21:11:41 -0400

    Added toggle to show text upon eventListener of click

--- app.js
+++ app.js
@@ -4,7 +4,8 @@
 const btns = document.querySelector(".question-btn");
 
 btns.forEach(function(btn) {
-    btn.addEventListner("click", function () {
-        console.log(e.currentTarget);
+    btn.addEventListner("click", function (e) {
+        const question = e.currentTarget.parentElement.parentElement;
+        question.classList.toggle("show-text");
     });
 });
