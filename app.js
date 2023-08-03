9fa0375e303ee35121d4eb96a3f655e82c5bbc81
0xsamuel-eth <sammmastenn@gmail.com>
2023-08-02 21:13:27 -0400

    Commented out method using traversing the dom

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 //using selectors inside the element
+
+
+
+
+
+
+
 // traversing the dom
 
-const btns = document.querySelector(".question-btn");
+// const btns = document.querySelector(".question-btn");
 
-btns.forEach(function(btn) {
-    btn.addEventListner("click", function (e) {
-        const question = e.currentTarget.parentElement.parentElement;
-        question.classList.toggle("show-text");
-    });
-});
+// btns.forEach(function(btn) {
+//     btn.addEventListner("click", function (e) {
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle("show-text");
+//     });
+// });
