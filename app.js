b3e34de54b6cd178caac7bb58113ae4e1e2c66aa
0xsamuel-eth <sammmastenn@gmail.com>
2023-08-02 21:03:29 -0400

    Added btn constant with eventListener for clicks

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 //using selectors inside the element
 // traversing the dom
 
+const btns = document.querySelector(".question-btn");
+
+btns.forEach(function(btn) {
+    btn.addEventListner("click", function () {
+        console.log(e.currentTarget);
+    });
+});
