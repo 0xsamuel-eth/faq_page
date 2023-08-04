237d8fb8e9d32981d6d995951bb9441fb5348e05
0xsamuel-eth <sammmastenn@gmail.com>
2023-08-03 22:13:15 -0400

    Created forEach to query select btn

--- app.js
+++ app.js
@@ -2,7 +2,11 @@
 
 const question = document.querySelectorAll(".question");
 
-
+questions.forEach(function(question) {
+    // console.log(question);
+    const btn = question.querySelector(".question-btn");
+    console.log(btn);
+});
 
 
 
